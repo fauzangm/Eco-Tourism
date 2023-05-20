70c48ae95477297b502baa4cfec4e0e85ab5bcc4
Fauzan Ghozi Mubarak <fauzanghozi56@gmail.com>
2023-05-20 11:41:58 +0700

    update model data

--- app/src/main/java/com/id/etourism/data/network/model/DataLokasiWisata.kt
+++
@@ -1,7 +0,0 @@
-package com.id.etourism.data.network.model
-
-
-data class DataLokasiWisata (
-
-        var dataloksai : List<DetailWisata>
-        )
--- app/src/main/java/com/id/etourism/data/network/model/DetailWisata.kt
+++
@@ -1,7 +0,0 @@
-package com.id.etourism.data.network.model
-
-data class DetailWisata (
-    var image : String,
-    var wisata : HashMap<String,String>
-        )
-
